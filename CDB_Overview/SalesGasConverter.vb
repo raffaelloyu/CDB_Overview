5dc9b0ef8a9d1e35772e1155b4a24269b854af01
yutecxa <yutecxa@PC14LZFH>
2022-11-09 10:06:13 +0800

    修改商品气流量和当日商品气累计产量单位

---
+++ CDB_Overview/SalesGasConverter.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Globalization
+
+Public Class SalesGasConverter
+    Implements IValueConverter
+
+    Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.Convert
+        Return Format(value * 100, "#.##")
+
+    End Function
+
+    Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+        Dim objTypeConverter As System.ComponentModel.TypeConverter = System.ComponentModel.TypeDescriptor.GetConverter(targetType)
+        Dim objReturnValue As Object = Nothing
+
+        If objTypeConverter.CanConvertFrom(value.[GetType]()) Then
+            objReturnValue = objTypeConverter.ConvertFrom(value)
+        End If
+
+        Return objReturnValue
+    End Function
+End Class
